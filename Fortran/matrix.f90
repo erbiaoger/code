bd37854b8bdac1ef070e1b31f954d1fb2f198f49
erbiaoger <erbiaoger@gmail.com>
2023-04-10 14:04:19 +0800

    vscode + fortran

--- Fortran/matrix.f90
+++ Fortran/matrix.f90
@@ -1,8 +1,8 @@
 program matrix
-
 	integer m(10)
 	real x(20), y(4, 5)
 	complex mat(5, 5)
+
 	
 	do i = 1, 10
 		m(i) = i
---
+++ Fortran/test1.f90
@@ -0,0 +1,9 @@
+program test1
+integer x, y, z
+write(*,*) "Hello, World!"
+x = 1
+y = 2
+z = x + 2*y
+write(*,*) "z = ", z
+read(*,*)
+end
